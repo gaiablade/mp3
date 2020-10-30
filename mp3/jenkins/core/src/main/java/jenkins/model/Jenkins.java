a77f0ffb755033c808b2317b562415989e53b007
gaiablade <cgandeg@gmail.com>
2020-10-30 16:36:05 -0400

    q3

--- mp3/jenkins/core/src/main/java/jenkins/model/Jenkins.java
+++ mp3/jenkins/core/src/main/java/jenkins/model/Jenkins.java
@@ -4245,4 +4245,9 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
         }
     }
 
+    public void updateAndTrim() {
+        this.updateComputerList();
+        this.trimLabels();
+    }
+
 }
